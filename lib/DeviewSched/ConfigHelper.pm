59c6285652b27db817ec6c24913dde8c1293e58a
unstabler <doping.cheese@gmail.com>
2015-07-31 03:37:40 +0900

    refactor(::ConfigHelper): 메소드명 변경(load_config)

--- lib/DeviewSched/ConfigHelper.pm
+++ lib/DeviewSched/ConfigHelper.pm
@@ -6,7 +6,7 @@ use warnings;
 require Exporter;
 
 our @ISA = qw/Exporter/;
-our @EXPORT_OK = qw/load_config_from_outside get_config_path/;
+our @EXPORT_OK = qw/load_config get_config_path/;
 
 sub get_config_path {
     if (defined $ENV{DEVIEWSCHED_BACKEND_CONF}) {
@@ -17,7 +17,7 @@ sub get_config_path {
 }
 
 
-sub load_config_from_outside {
+sub load_config {
     
     # 설정 파일을 어플리케이션 외부(크롤링 스크립트 등)에서 불러올 수 있도록 합니다.
     # FIXME: 설정 파일 로드할려고 Mojolicious::Lite를 외부 스크립트 내에서 불러오는게
@@ -27,7 +27,7 @@ sub load_config_from_outside {
     my $config_path = get_config_path;
 
     if (-e $config_path) {
-        $app->plugin(config => {file => $config_path});
+        $app->plugin(config  => {file => $config_path});
     } else {
         warn sprintf "WARN: configuration file (%s) not exists", $config_path;
     }

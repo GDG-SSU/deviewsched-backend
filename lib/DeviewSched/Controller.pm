f55c27d9939396d565a9c25dd30124e006edd657
unstabler <doping.cheese@gmail.com>
2015-08-31 21:03:33 +0900

    fix(::Controller): 201 리턴시 is_success가 false인 문제 수정

--- lib/DeviewSched/Controller.pm
+++ lib/DeviewSched/Controller.pm
@@ -5,14 +5,19 @@ use Mojo::Base 'Mojolicious::Controller';
 sub FAIL_RESOURCE_NOT_AVAILABLE       () { (404, "The request resource is not available") }
 sub FAIL_INVALID_AUTHORIZATION_HEADER () { (401, "Invalid Authorization Header") }
 sub FAIL_AUTHORIZATION_FAILED         () { (401, "Authorization Failed")  }
-
 sub FAIL_INVALID_TOKEN                () { (401, "Invalid Token") }
 sub FAIL_TOKEN_EXPIRED                () { (401, "Token Expired") }
-
 sub FAIL_BAD_PARAMETERS               () { (401, "Bad Parameters") }
+
+
+sub FAIL_SCHEDULE_INSERTION           () { (401, "Schedule Insertion Failed") }
+sub FAIL_SCHEDULE_NOT_FOUND           () { (401, "Schedule not found") }
+
+sub FAIL_UNKNOWN_ERROR                () { (501, "Unknown error" ) }
 
 sub FAIL_REASON_MALFORMED_JSON        () { "Malformed JSON" }
 sub FAIL_REASON_AUTHORIZATION_HEADER  () { "Authorization header not defined" }
+
 
 sub fail {
     my $self = shift;
@@ -21,9 +26,9 @@ sub fail {
     $self->app->log->error(
         (sprintf "%s %s:", $self->req->method, $self->req->url),
         $log
-    ) if defined $log;
+    ) if $log;
 
-    $reason .= sprintf(" (%s)", $detail) if defined $detail;
+    $reason .= sprintf(" (%s)", $detail) if $detail;
     
     $self->render_wrap($code, { reason => $reason });
     return undef;
@@ -36,11 +41,11 @@ sub render_wrap {
     # 좀 더 깔끔하게 못하나!
     $code = $self->stash('override_return_code') if $self->stash('override_return_code');
 
-    my $is_success = ($code / 100 == 2) ? Mojo::JSON->true : Mojo::JSON->false;
+    my $is_success = (int($code / 100) == 2) ? Mojo::JSON->true : Mojo::JSON->false;
 
     return $self->render(status => $code, json => { 
         is_success => $is_success, 
-        (%{ $content }) 
+        (%{ $content || {} }) 
     }); 
 }
 
